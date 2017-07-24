275cdc3f8e0f1cd0d3c7ce1f7ac20e599f46dcd1
Moch. Lutfi <kapten.lufi@gmail.com>
2017-07-24 16:40:50 +0700

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="layout">
+    <div class="header">
+      <div class="logo">
+        Muse-UI
+      </div>
+      <div class="nav">
+        <mu-tabs :value="activeTab" @change="handleTabChange" class="tab">
+          <mu-tab value="tab1" title="Nav One"/>
+          <mu-tab value="tab2" title="Nav TwO"/>
+          <mu-tab value="tab3" title="Nav Three"/>
+        </mu-tabs>
+      </div>
+    </div>
+    <div class="content">
+      <div class="breadcrumb">
+        <mu-breadcrumb>
+          <mu-breadcrumb-item href="/">Home</mu-breadcrumb-item>
+          <mu-breadcrumb-item href="/">VideoGame</mu-breadcrumb-item>
+          <mu-breadcrumb-item>Download</mu-breadcrumb-item>
+        </mu-breadcrumb>
+      </div>
+      <div class="body">
+        <nuxt/>
+      </div>
+    </div>
+    <div class="footer">
+      Muse-UI ©2017 Created by Muse-UI
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      activeTab: 'tab1'
+    }
+  },
+  methods: {
+    handleTabChange (val) {
+      this.activeTab = val
+    }
+  }
+}
+</script>
+<style scoped>
+.layout{
+  background-color: rgb(236, 236, 236);
+}
+
+.header{
+  background-color: #7e57c2;
+}
+
+.logo{
+  font-size: 24px;
+  color: white;
+  display: inline-block;
+  padding: 10px 20px;
+}
+
+.nav{
+  display: inline-block;
+  width: calc(100% - 150px);
+  margin: 0 auto;
+}
+
+.tab{
+  margin: 0 auto;
+  width: 400px;
+  background-color: rgba(0, 0, 0, 0);
+}
+
+.content{
+  width: 90%;
+  margin: 0 auto;
+}
+
+.breadcrumb{
+  margin: 10px 0;
+}
+
+.body{
+  background-color: white;
+  border-radius: 5px;
+  min-height: 500px;
+}
+
+.footer{
+  padding: 20px 0;
+  text-align: center;
+}
+</style>
